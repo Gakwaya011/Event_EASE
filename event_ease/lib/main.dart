076e78180b56e0198033688c1e45e845265234dc
Cchancee <c.karambizi@alustudent.com>
2025-03-27 08:55:10 +0200

    added firebase

--- event_ease/lib/main.dart
+++ event_ease/lib/main.dart
@@ -6,10 +6,18 @@ import 'pages/onboarding.dart';
 import 'pages/profile.dart';
 import 'pages/splash.dart';
 import 'pages/otp.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; 
 
-void main() {
-  runApp(const MyApp());
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget { 
   const MyApp({super.key});
