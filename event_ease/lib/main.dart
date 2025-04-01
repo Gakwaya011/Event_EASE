70c18e383036029b934a18393b8cf6aed030fe84
Cchancee <c.karambizi@alustudent.com>
2025-04-01 11:22:13 +0200

    ask notification permission

--- event_ease/lib/main.dart
+++ event_ease/lib/main.dart
@@ -1,33 +1,56 @@
 import 'package:flutter/material.dart';
-// ignore: unused_import
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:permission_handler/permission_handler.dart'; // Add this import
 import 'package:provider/provider.dart';
-import 'core/navigation/router.dart';
 import 'core/providers/auth_provider.dart';
 import 'core/providers/event_provider.dart';
 import 'firebase_options.dart';
+import 'core/navigation/router.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print("Handling background message: ${message.messageId}");
+}
 
+Future<void> requestPermission() async {
+  final status = await Permission.notification.request();
+  if (status.isGranted) {
+    print("Notification Permission Granted");
+  } else {
+    print("Notification Permission Denied");
+  }
+}
+
+FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+Future<void> getFCMToken() async {
+  String? token = await messaging.getToken();
+  print("FCM Token: $token");
+}
 
 Future<void> main() async {
-  
-
-WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
 
   if (kIsWeb) {
-    // ✅ Web requires FirebaseOptions
     await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   } else {
-    // ✅ Mobile initializes normally
     await Firebase.initializeApp();
   }
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  // Request notification permission
+  await requestPermission();
+
+  // Get FCM token
+  await getFCMToken();
 
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => AuthProvider()), // Provide AuthProvider
-        ChangeNotifierProvider(create: (_) => EventProvider()), // Event AuthProvider
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => EventProvider()),
       ],
       child: const MyApp(),
     ),
@@ -41,8 +64,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      routerConfig: router, // Use GoRouter for navigation
-     
+      routerConfig: router,
     );
   }
 }
